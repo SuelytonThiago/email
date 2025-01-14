f31a1919b86d2019cfb103b964fd3033a4a3cc31
Suelyton Thiago <suelytonsouza0202@gmail.com>
2025-01-13 23:04:21 -0300

    adding email validation to the sendEmail method of the EmailSender class

--- src/main/java/com/ms/email/rest/services/EmailService.java
+++ src/main/java/com/ms/email/rest/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.ms.email.rest.services;
 import com.ms.email.rest.dto.EmailDto;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,7 +14,18 @@ public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
 
+
+
     public void sendEmail(EmailDto dto) throws MessagingException {
+
+        if (!isValidEmail(dto.getEmailTo())) {
+            throw new IllegalArgumentException("Endereço de e-mail inválido: " + dto.getEmailTo());
+        }
+
+        if (!isValidEmail(dto.getEmailFrom())) {
+            throw new IllegalArgumentException("Endereço de e-mail do remetente inválido: " + dto.getEmailFrom());
+        }
+
         MimeMessage message = emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
@@ -23,5 +35,15 @@ public class EmailService {
         helper.setFrom(dto.getEmailFrom());
 
         emailSender.send(message);
+    }
+
+    private boolean isValidEmail(String email) {
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+            return true;
+        } catch (jakarta.mail.internet.AddressException ex) {
+            return false;
+        }
     }
 }
